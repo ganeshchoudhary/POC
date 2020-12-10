7d8497bcbd875001ef86c84d0c093fb00ff630a2
Ganesh Singh <ganesh.singh@hashedin.com>
2020-12-10 17:24:59 +0530

    POC is completed

---
+++ src/main/java/com/rupeek/poc/poc/PocApplication.java
@@ -0,0 +1,15 @@
+package com.rupeek.poc.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(VariableConfiguration.class)
+public class PocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocApplication.class, args);
+	}
+
+}
